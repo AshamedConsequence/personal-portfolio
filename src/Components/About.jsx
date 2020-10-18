badcbc6b1dfc09dc2dc9ec565edb7052b57808cc
Maxime Morize <morizem@awty.org>
2020-10-18 12:10:51 -0500

    changed colors and fixed alignments

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,21 +7,25 @@ function About() {
         <h2>About Me</h2>
 
         <div className="info-grid">
-          <img
-            className="logo"
-            id="profile-pic"
-            src="/profile_pic.png"
-            alt="emacs"
-          />
+          <div className="logo">
+            <img id="profile-pic" src="/profile_pic.png" alt="emacs" />
+            <p>The 3 buttons, which will become the theme selector</p>
+          </div>
           <div className="me">
-            <h3>Who I am</h3>
-            <p>
-              I am a highschool student first, developper second and co-lead for
-              my school's hackclub. My love for Vim and Emacs are unparalleled.
-              As the co-lead, I spend most of my time there teaching the newer
-              kids how to program. I enjoy minimalism and try to make my
-              codebase as small as practically possible.
-            </p>
+            <div id="card">
+              <h3>Who I am</h3>
+              <div id="tl" className="corner"></div>
+              <div id="tr" className="corner"></div>
+              <p>
+                I am a highschool student first, developper second and co-lead
+                for my school's hackclub. My love for Vim and Emacs are
+                unparalleled. As the co-lead, I spend most of my time there
+                teaching the newer kids how to program. I enjoy minimalism and
+                try to make my codebase as small as practically possible.
+              </p>
+              <div id="bl" className="corner"></div>
+              <div id="br" className="corner"></div>
+            </div>
           </div>
           <div className="knowledge">knowledge</div>
           <div className="projects">projects</div>
